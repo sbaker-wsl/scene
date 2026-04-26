a95a3c6495dfebfa96dac6a8ef433df9f8ef8d59
sbaker-wsl <snb93@pitt.edu>
2026-04-26 16:07:49 -0400

    stupid

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,5 +4,6 @@ import { getUser } from '@/lib/getUser';
 
 export default async function TestPage() {
 	const user = await getUser();
+	console.log(user);
 	return <div className = "text-white">{user.username}</div>;
 }
