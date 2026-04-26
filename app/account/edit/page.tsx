614e455a161a6265811005b58cba038556b4d91a
sbaker-wsl <snb93@pitt.edu>
2026-04-26 19:54:01 -0400

    fix bad coloring + center edit form

--- app/account/edit/page.tsx
+++ app/account/edit/page.tsx
@@ -2,7 +2,7 @@ import EditForm from './EditForm'
 
 export default function EditAccountPage() {
     return (
-        <div className = "flex flex-col items-center gap-6 bg-black min-h-screen">
+        <div className = "flex flex-col items-center justify-center gap-6 bg-black min-h-screen">
             <EditForm />
         </div>
     )
