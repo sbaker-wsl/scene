912a2e43edfd447207dd2fc2dae1cf308b2a9b38
sbaker-wsl <snb93@pitt.edu>
2026-04-26 16:15:46 -0400

    made it so user is returned rather than the result of the jwt verify function, so pages can use the object for dynamic filling in

--- lib/getUser.ts
+++ lib/getUser.ts
@@ -1,10 +1,14 @@
 import { cookies } from 'next/headers'
 import jwt from 'jsonwebtoken'
+import User from '@/models/User'
 
 export async function getUser() {
     const cookieStore = await cookies();
     const token = cookieStore.get('token')?.value
     if (!token) return null
 
-    return jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await User.findById(decoded.userId).select('-password')
+    return user;
 }
