f9ca61ab0e07772e794f4aed6bd9532b525ec210
BilalDH <BDH67@pitt.edu>
2026-04-26 22:10:59 -0400

    Working on home page, commented out venue.ts

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -643,38 +643,3 @@ export default function VenuesPage() {
 }
 
 
-/*
-class User {
-    private name : string;
-    private email: string;
-
-    constructor(name: string, email: string) {
-        this.name = name;
-        this.email = email;
-    
-    }
-
-    getName(): string {
-        return this.name;
-    }
-}
-
-export default function VenuesPage() {
-    const user = new User("nothin so far", "sweetums!@gmail.com");
-    return (
-        <div className="min-h-screen bg-gray-100 p-8">
-            <h1 className="text-4xl font-bold text-center mb-6">Music Venues</h1>
-            <p className="text-center text-gray-600">Discover music venues near you!</p>
-
-            <div className="mt-10 grid gird-cols-1 md:grid
-            -cols-2 lg:grid-cols-3 gap-6">
-                <div className="p-4 bg-white rounded shadow">Venue 1</div>
-                <div className="p-4 bg-white rounded shadow">Venue 2</div>
-                <div className="p-4 bg-white rounded shadow">Venue 3</div>
-                <div className="p-4 bg-white rounded shadow">{user.getName()}</div>
-            </div>
-        </div>
-    )
-}
-
-*/
