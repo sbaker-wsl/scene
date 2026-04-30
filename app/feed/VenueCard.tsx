0e249deb2f516e3c7889f806b2df8ffe7d656a54
BilalDH <BDH67@pitt.edu>
2026-04-30 18:30:24 -0400

    Added a button to feed that lets you copy the artists contact number if it is provided

--- app/feed/VenueCard.tsx
+++ app/feed/VenueCard.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+
 export function VenueCard( { venue }: any) {
     const date = new Date(venue.date);
+    const [copied, setCopied] = useState(false);
+
+    const handleContact =() => {
+        navigator.clipboard.writeText(venue.contact);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+    }
 
     return (
         <div className="text-center space-y-4">
@@ -11,6 +20,15 @@ export function VenueCard( { venue }: any) {
                 {date.toLocaleDateString()} <br />
                 {date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit"})}
             </p>
+
+            {venue.contact && (
+                <button
+                    onClick = {handleContact}
+                    className="mt-2 px-6 py-2 bg-white text-black rounded-full font-semibold hover:bg-gray-200 transition"
+                >
+                    {copied ? "Copied!" : "Copy Contact"}
+                </button>
+            )}
         </div>
     );
 }
